5bfee794c30fce606f2d804cccb9ddcca72d1a2a
Sven Fu <xinwen.fu@cloudminds.com>
2020-08-19 15:36:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# vulcan-id
+
+> No more sacrifices, no more gods. Use your strength, Vulcan.
+>
+> 再不会有牺牲了，也不会有神了，善用你的力量吧，赫淮斯托斯。
+
+
+## Introduction
+
+vulcan-id是一套高性能ID生成服务。
+
+满足ID生成的三大核心需求：
+
+- 全局唯一
+- 按照时间粗略有序
+- 尽可能短
+
---
+++ src/main/java/com/vulcan/id/VulcanIdApplication.java
@@ -0,0 +1,16 @@
+package com.vulcan.id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动器
+ */
+@SpringBootApplication
+public class VulcanIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VulcanIdApplication.class, args);
+    }
+
+}
